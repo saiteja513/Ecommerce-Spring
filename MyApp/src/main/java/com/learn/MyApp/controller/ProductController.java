301740de5a454993972781f6c97390e331919c56
Kolluru Saiteja <steja3351@gmail.com>
2024-09-30 00:30:52 +0530

    new update

--- MyApp/src/main/java/com/learn/MyApp/controller/ProductController.java
+++ MyApp/src/main/java/com/learn/MyApp/controller/ProductController.java
@@ -3,9 +3,7 @@ package com.learn.MyApp.controller;
 import com.learn.MyApp.model.Product;
 import com.learn.MyApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,4 +23,15 @@ public class ProductController {
     public Product getProductById(@PathVariable int prodId){
         return service.getProductById(prodId);
     }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product prod){
+        service.addProduct(prod);
+    }
+
+    @PutMapping("/products")
+    public void updateProduct(@RequestBody Product prod){
+        service.updateProduct(prod);
+    }
+
 }
--- MyApp/src/main/java/com/learn/MyApp/service/ProductService.java
+++ MyApp/src/main/java/com/learn/MyApp/service/ProductService.java
@@ -4,15 +4,16 @@ package com.learn.MyApp.service;
 import com.learn.MyApp.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>(Arrays.asList(
             new Product(101,"Samsung",50000),
-            new Product(102,"MSI",60000));
+            new Product(102,"MSI",60000)));
 
     public List<Product> getProducts(){
         return products;
@@ -24,4 +25,16 @@ public class ProductService {
                 .findFirst().get();
     }
 
+    public void addProduct(Product prod){
+        products.add(prod);
+    }
+
+    public void updateProduct(Product prod) {
+        int index = 0;
+        for(int i=0;i<products.size();i++){
+            if(products.get(i).getProdId() == prod.getProdId()){
+                products.set(index, prod);
+            }
+        }
+    }
 }
